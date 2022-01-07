672939eed44fb087eb5401383f0d7b8bd7799521
Julio Magalhaes <julio.magalhaes@maps.com.br>
2022-01-06 23:19:57 -0300

    Cartões dos projetos salvos

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,10 +24,35 @@ function searchToggle() {
 
 function inputColor() {
   const inputColor = document.querySelector(".js-input-color").value;
-  const moldColor = document.querySelector(".js-mold")
-  const color = '--bg-color: ' + inputColor
+  const moldColor = document.querySelector(".js-mold");
+  const color = "--bg-color: " + inputColor;
 
-  moldColor.setAttribute('style', color)
+  moldColor.setAttribute("style", color);
 
   console.log(moldColor, inputColor);
 }
+
+const linguagem = document.querySelector(".js-linguagem");
+const areaDoCodigo = document.querySelector(".js-editor");
+const botao = document.querySelector(".js-hljs");
+
+function aplicaHighlight() {
+  const codigo = areaDoCodigo.innerText;
+  areaDoCodigo.innerHTML = `<code class="hljs ${linguagem.value}" contenteditable="true" aria-label="Editor de código"></code>`;
+  areaDoCodigo.querySelector("code").textContent = codigo;
+  hljs.highlightElement(areaDoCodigo.querySelector("code"));
+}
+
+if (botao) {
+  botao.addEventListener("click", () => {
+    aplicaHighlight();
+  });
+}
+
+const feed = document.querySelector(".js-feed-item");
+const slideDown = document.getElementsByClassName("js-slide-down");
+const width = feed.clientWidth - 48
+
+const test = Array.prototype.filter.call(slideDown, (r) => {
+  r.setAttribute('style', 'width: ' + width + 'px')
+})
